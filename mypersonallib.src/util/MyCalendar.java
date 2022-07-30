d9f266d100e0f7f446ad16f15b34040495fed76f
Steve Nunes da Silva <stevenunes@gmail.com>
2022-07-29 22:27:24 -0300

    Update

--- mypersonallib.src/util/MyCalendar.java
+++ mypersonallib.src/util/MyCalendar.java
@@ -6,6 +6,8 @@ import java.time.ZoneId;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import enums.PeriodToMillis;
+
 public class MyCalendar {
 	
 	/*
@@ -239,5 +241,65 @@ public class MyCalendar {
 	
 	public static String getIntervaloEmDiasEntreDatasStr(Date date)
 		{ return getIntervaloEmDiasEntreDatasStr(date, new Date()); }
+	
+	/**
+	 * Informando um valor em milisegundos, retorna uma array de int onde cada casa contém a duração em [DI][HO][MIN][SEG]
+	 * @param cTimeDuration		Duração em milisegundos á converter em DD:HH:MM:SS
+	 * @param show		0/1 especificando as casas que serão exibidas<br>
+	 * <b>Ex:</b><br>
+	 * "1111" - DD:HH:MM:SS<br>
+	 * "0111" - HH:MM:SS<br>
+	 * "0110" - HH:MM<br><br>
+	 * Se for especificado 'x' no lugar de '1/0', significa que aquele campo não deve contabilizar no calculo.<br>
+	 * Ex:<br>
+	 * Se especificar duração suficiente para 36 horas, e informar em <b>show</b> "1111" retornará [1][24][0][0]<br>
+	 * Se especificar duração suficiente para 36 horas, e informar em <b>show</b> "x111" retornará [0][36][0][0]<br>
+	 * Se especificar duração suficiente para 36 horas, e informar em <b>show</b> "xx11" retornará [0][0][2160][0]<br>
+	 * @return		Array de int contendo a duração em [DI][HO][MIN][SEG]
+	 */
+	public static int[] duration(long cTimeDuration, String show) {
+		int[] dur = new int[4];
+		long[] millis = {PeriodToMillis.DAY.getValue(), PeriodToMillis.HOUR.getValue(), PeriodToMillis.MINUTE.getValue(), PeriodToMillis.SECOND.getValue()};
+		for (int n = 0; n < 4; n++) {
+			if (show.charAt(n) != 'x') {
+				dur[n] = (int)(cTimeDuration / millis[n]);
+				cTimeDuration -= millis[n] * dur[n];
+			}
+		}
+		return dur;
+	}
+	
+	public static int[] duration(long cTimeDuration)
+		{ return duration(cTimeDuration, "1111"); }
+
+	/**
+	 * Informando um valor em milisegundos, retorna a duração no formato DD:HH:MM:SS
+	 * @param cTimeDuration		Duração em milisegundos á converter em DD:HH:MM:SS
+	 * @param show		0/1 especificando as casas que serão exibidas<br>
+	 * <b>Ex:</b><br>
+	 * "1111" - DD:HH:MM:SS<br>
+	 * "0111" - HH:MM:SS<br>
+	 * "0110" - HH:MM<br>
+	 * Se for especificado 'x' no lugar de '1/0', significa que aquele campo não deve contabilizar no calculo.<br>
+	 * Ex:<br>
+	 * Se especificar duração suficiente para 36 horas, e informar em <b>show</b> "1111" retornará 01:24:00:00<br>
+	 * Se especificar duração suficiente para 36 horas, e informar em <b>show</b> "x111" retornará 36:00:00<br>
+	 * Se especificar duração suficiente para 36 horas, e informar em <b>show</b> "xx11" retornará 2160:00<br>
+	 * @return		Duração no formato DD:HH:MM:SS
+	 */
+	public static String durationStr(long cTimeDuration, String show) {
+		int[] dur = duration(cTimeDuration, show);
+		StringBuilder result = new StringBuilder();
+		for (int n = 0; n < 4; n++)
+			if (show.charAt(n) == '1' && (n > 0 || dur[0] > 0)) {
+				if (!result.isEmpty())
+					result.append(":");
+				result.append(Misc.fillWithZerosAtLeft("" + dur[n], 2));
+			}
+		return result.toString();
+	}
+	
+	public static String durationStr(long cTimeDuration)
+		{ return durationStr(cTimeDuration, "1111"); }
 
 }
