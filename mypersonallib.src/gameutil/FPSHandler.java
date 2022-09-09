5a2efc386f0b8ef835eccfda12d0c7b9cbe5d273
Steve Nunes da Silva <stevenunes@gmail.com>
2022-09-09 19:34:33 -0300

    Classe FPSHandler adicionada.

---
+++ mypersonallib.src/gameutil/FPSHandler.java
@@ -0,0 +1,76 @@
+package gameutil;
+
+public class FPSHandler {
+
+	private int gameCyclesPerSecond;
+	private long nextCicleAt;
+	private int gameFrameSkip;
+	private int frameSkip;
+	private long elapsedFrames;
+	private int fps;
+	private int cps;
+	private long fpsTimer;
+	private int currentFPS;
+	private int currentCPS;
+	
+	public FPSHandler(int cyclesPerSecond, int frameSkip) {
+		gameCyclesPerSecond = cyclesPerSecond;
+		gameFrameSkip = frameSkip;
+		frameSkip = 0;
+		elapsedFrames = 0;
+		fps = 0;
+		cps = 0;
+		nextCicleAt = System.currentTimeMillis();
+		fpsTimer = System.currentTimeMillis();
+		currentFPS = 0;
+		currentCPS = 0;
+	}
+
+	public void setFPS(int fps)
+		{ gameCyclesPerSecond = fps; }
+
+	public void setFrameSkip(int frameSkip)
+		{ this.gameFrameSkip = frameSkip; }
+	
+	public int getFPS()
+		{ return currentFPS; }
+
+	public int getCPS()
+		{ return currentCPS; }
+
+	public long getElapsedFrames()
+		{ return elapsedFrames; }
+
+	/**
+	 * Call this method on your main loop every frame.
+	 */
+	private void fpsCounter() {
+		if (gameCyclesPerSecond > 0) {
+			while (System.currentTimeMillis() < nextCicleAt)
+				KeyHandler.runItOnMainLoopEveryFrame();
+			nextCicleAt += 1000 / gameCyclesPerSecond;
+		}
+		frameSkip++;
+		elapsedFrames++;
+		cps++;
+		if (ableToDraw()) {
+			frameSkip = 0;
+			fps++;
+		}
+		if (System.currentTimeMillis() >= fpsTimer) {
+			fpsTimer += 1000;
+			currentFPS = fps;
+			currentCPS = cps;
+			fps = 0;
+			cps = 0;
+		}
+	}
+	
+	/*
+	 * Only update your screen when this method returns {@code true},
+	 * otherwise, just update game stuffs without any drawning. 
+	 */
+	private Boolean ableToDraw()
+		{ return gameFrameSkip == 0 || frameSkip >= gameFrameSkip; }
+
+}
