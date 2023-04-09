daa2dd1dcc3767a8b81d64b811dd62434737186f
Steve Nunes da Silva <stevenunes@gmail.com>
2023-04-09 14:06:38 -0300

    AAdicionado métodos para linkar um objeto da classe 'EliticMove' a uma position, permitindo que o objeto fique circulando ao redor de outro objeto mesmo que o objeto se mova

--- mypersonallib.src/gameutil/EliticMove.java
+++ mypersonallib.src/gameutil/EliticMove.java
@@ -4,6 +4,7 @@ public class EliticMove {
 	
 	private Position centerPosition, currentPosition;
 	private double radiusW, radiusH, speed, angle = 0;
+	private Object linkedPosition;
 
 	public EliticMove(Position centerPosition, double radiusW, double radiusH, double speed) {
 		this.centerPosition = new Position(centerPosition);
@@ -11,6 +12,7 @@ public class EliticMove {
 		this.radiusW = radiusW;
 		this.radiusH = radiusH;
 		this.speed = speed;
+		linkedPosition = null;
 	}
 
 	public void move() {
@@ -21,5 +23,14 @@ public class EliticMove {
 
 	public Position getPosition()
 		{ return currentPosition; }
+	
+	public void setCenterPosition(Position position)
+		{ centerPosition.setPosition(position); }
+	
+	public void linkCenterPositionToPosition(Position position)
+		{ linkedPosition = position; }
+	
+	public void unlinkCenterPosition()
+		{ linkedPosition = null; }
 
 }
