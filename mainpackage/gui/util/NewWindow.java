222515f7cf2a5b28569b3827ee1a41b2db00b7d8
Steve Nunes <stevenunes@gmail.com>
2023-11-11 13:44:26 -0300

    Fixado problemas com a Classe 'NewWindow'

--- mainpackage/gui/util/NewWindow.java
+++ mainpackage/gui/util/NewWindow.java
@@ -3,7 +3,10 @@ package gui.util;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Window;
 
 public class NewWindow {
 	
@@ -13,18 +16,43 @@ public class NewWindow {
 	@SuppressWarnings("unused")
 	private NewWindow() {}
 	
-	public NewWindow(String fxml) {
+	public NewWindow(Window parentWindow, Modality modality, StageStyle style, String fxml) {
 		try {
 			loader = new FXMLLoader(NewWindow.class.getResource(fxml));
 			Pane pane = loader.load();
 			stage = new Stage();
 			stage.setScene(new Scene(pane));
+			if (parentWindow != null)
+				stage.initOwner(parentWindow);
+			if (modality != null)
+				stage.initModality(modality);
+			if (style != null)
+				stage.initStyle(style);
 			stage.show();
 		}
 		catch (Exception e)
 			{ throw new RuntimeException("Unable to create window"); }
 	}
 
+	public NewWindow(Window parentWindow, Modality modality, String fxml)
+		{ this(parentWindow, modality, null, fxml); }
+
+	public NewWindow(Window parentWindow, StageStyle style, String fxml)
+		{ this(parentWindow, null, style, fxml); }
+	
+	public NewWindow(Window parentWindow, String fxml)
+		{ this(parentWindow, null, null, fxml); }
+	
+	public NewWindow(Modality modality, String fxml)
+		{ this(null, modality, null, fxml); }
+	
+	public NewWindow(StageStyle style, String fxml)
+		{ this(null, null, style, fxml); }
+	
+	public NewWindow(String fxml)
+		{ this(null, null, null, fxml); }
+
+	
 	public Stage getStage()
 		{ return stage; }
 	
