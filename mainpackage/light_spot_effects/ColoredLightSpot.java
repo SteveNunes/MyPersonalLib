43ea1b7138c4787fb767cdef09bf017e4a42b1ff
Steve Nunes da Silva <stevenunes@gmail.com>
2024-09-27 01:18:37 -0300

    Fixes & Updates

---
+++ mainpackage/light_spot_effects/ColoredLightSpot.java
@@ -0,0 +1,146 @@
+package light_spot_effects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
+import objmoveutils.Position;
+import util.MyMath;
+
+public class ColoredLightSpot extends LightSpot {
+	
+	private static List<ColoredLightSpot> spots = new ArrayList<>();
+	private static List<ColoredLightSpot> tempSpots = new ArrayList<>();
+
+	private Color color;
+	private double opacity;
+
+	public ColoredLightSpot()
+		{ this(0, 0); }
+	
+	public ColoredLightSpot(int x, int y)
+		{ this(x, y, 10, Color.WHITE); }
+
+	public ColoredLightSpot(int x, int y, double radius)
+		{ this(x, y, radius, Color.WHITE); }
+	
+	public ColoredLightSpot(int x, int y, double radius, double opacity)
+		{ this(x, y, radius, Color.WHITE, opacity); }
+	
+	public ColoredLightSpot(int x, int y, double radius, Color color)
+		{ this(x, y, radius, color, 0.6); }
+	
+	public ColoredLightSpot(int x, int y, double radius, Color color, double opacity)
+		{ this(new Position(x, y), radius, color, 0.6); }
+	
+	public ColoredLightSpot(Position position)
+		{ this(position, 10, Color.WHITE); }
+
+	public ColoredLightSpot(Position position, double radius)
+		{ this(position, radius, Color.WHITE); }
+
+	public ColoredLightSpot(Position position, double radius, double opacity)
+		{ this(position, radius, Color.WHITE, opacity); }
+
+	public ColoredLightSpot(Position position, double radius, Color color)
+		{ this(position, radius, color, 0.6); }
+	
+	public ColoredLightSpot(Position position, double radius, Color color, double opacity) {
+		super(position, radius);
+		super.getPosition().setPosition(position.getX() / 2, position.getY() / 2);
+		this.color = color;
+		this.opacity = opacity;
+	}
+	
+	public double getOpacity()
+		{ return opacity; }
+	
+	public ColoredLightSpot setOpacity(double opacity) {
+		this.opacity = opacity;
+		return this;
+	}
+	
+	public Color getColor()
+		{ return color; }
+	
+	public ColoredLightSpot setColor(Color color) {
+		this.color = color;
+		return this;
+	}
+	
+	public ColoredLightSpot setOffsetX(int offsetX) {
+		super.setOffsetX(offsetX); 
+		return this;
+	}
+	
+	public ColoredLightSpot setOffsetY(int offsetY) {
+		super.setOffsetY(offsetY); 
+		return this;
+	}
+
+	public ColoredLightSpot setOffset(int offsetX, int offsetY) {
+		super.setOffset(offsetX, offsetY); 
+		return this;
+	}
+	
+	@Override
+	public int getX() {
+		int x = (int)position.getX();
+		if (xVariance != 0)
+			x = (int)MyMath.getRandom(x - xVariance, x + xVariance);
+		return x;
+	}
+	
+	@Override
+	public int getY() {
+		int y = (int)position.getY();
+		if (yVariance != 0)
+			y = (int)MyMath.getRandom(y - yVariance, y + yVariance);
+		return y;
+	}
+	
+	public ColoredLightSpot setRadiusVariance(double minRadius, double maxRadius, double incRadiusAmount) {
+		super.setRadiusVariance(minRadius, maxRadius, incRadiusAmount);
+		return this;
+	}
+	
+	public ColoredLightSpot setSpotVariance(int xVariance, int yVariance) {
+		super.setSpotVariance(xVariance, yVariance);
+		return this;
+	}
+	
+	public ColoredLightSpot setRadius(double radius) {
+		this.setRadius(radius);
+		return this;
+	}
+	
+	public static void clearTempColoredLightSpots()
+		{ tempSpots.clear(); }
+	
+	public static void addTempColoredLightSpot(ColoredLightSpot spot)
+		{ tempSpots.add(spot); }
+
+	public static void addColoredLightSpot(ColoredLightSpot spot)
+		{ spots.add(spot); }
+	
+	public static void removeColoredLightSpot(ColoredLightSpot spot)
+		{ spots.remove(spot); }
+
+	public static void setMultipleColoredLightSpots(GraphicsContext gc) {
+		gc.save();
+    for (int n = 0; n < 2; n++)
+	    for (ColoredLightSpot spot : n == 0 ? spots : tempSpots) {
+	    	int x = spot.getX() + spot.offsetX, y = spot.getY() + spot.offsetY;
+				RadialGradient gradient = new RadialGradient(0, 0, x, y, spot.getRadius(), false, CycleMethod.NO_CYCLE, new Stop(0, spot.getColor()), new Stop(1, Color.TRANSPARENT));
+				gc.setFill(gradient);
+				gc.setGlobalAlpha(spot.getOpacity());
+				gc.fillOval(x - spot.getRadius(), y - spot.getRadius(), spot.getRadius() * 2, spot.getRadius() * 2);
+	    }
+		gc.restore();
+	}
+
+}
---
+++ mainpackage/light_spot_effects/LightSpot.java
@@ -0,0 +1,159 @@
+package light_spot_effects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.Blend;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.Light;
+import javafx.scene.effect.Lighting;
+import objmoveutils.Position;
+import util.MyMath;
+
+public class LightSpot {
+	
+	private static List<LightSpot> spots = new ArrayList<>();
+	private static List<LightSpot> tempSpots = new ArrayList<>();
+	private static List<LightSpot> spotsInDarkness = new ArrayList<>();
+
+	Position position;
+	int offsetX;
+	int offsetY;
+	int xVariance;
+	int yVariance;
+	double radius;
+	double minRadius;
+	double maxRadius;
+	double incRadius;
+	Light.Point lightPoint;
+	
+	public LightSpot()
+		{ this(0, 0); }
+
+	public LightSpot(int x, int y)
+		{ this(x, y, 10); }
+	
+	public LightSpot(int x, int y, double radius)
+		{ this(new Position(x, y), radius); }
+
+	public LightSpot(Position position)
+		{ this(position, 10); }
+	
+	public LightSpot(Position position, double radius) {
+		this.position = position;
+		lightPoint = new Light.Point();
+		setRadiusVariance(radius, radius, 0);
+		setSpotVariance(0, 0);
+		setOffset(0, 0);
+	}
+	
+	public LightSpot setRadiusVariance(double minRadius, double maxRadius, double incRadiusAmount) {
+		radius = minRadius;
+		this.minRadius = minRadius;
+		this.maxRadius = maxRadius;
+		incRadius = incRadiusAmount;
+		return this;
+	}
+	
+	public LightSpot setSpotVariance(int xVariance, int yVariance) {
+		this.xVariance = xVariance;
+		this.yVariance = yVariance;
+		return this;
+	}
+	
+	public Light.Point getLightPoint() {
+		lightPoint.setX(getX() + offsetX);
+		lightPoint.setY(getY() + offsetY);
+		lightPoint.setZ(getRadius());
+		return lightPoint;
+	}
+	
+	public Position getPosition()
+		{ return position; }
+	
+	public int getX() {
+		int x = (int)position.getX() * 2;
+		if (xVariance != 0)
+			x = (int)MyMath.getRandom(x - xVariance, x + xVariance);
+		return x;
+	}
+	
+	public int getY() {
+		int y = (int)position.getY() * 2;
+		if (yVariance != 0)
+			y = (int)MyMath.getRandom(y - yVariance, y + yVariance);
+		return y;
+	}
+	
+	public double getRadius() {
+		if (incRadius > 0 && (radius += incRadius) >= maxRadius) {
+			radius = maxRadius;
+			incRadius = -incRadius;
+		}
+		else if (incRadius < 0 && (radius += incRadius) <= minRadius) {
+			radius = minRadius;
+			incRadius = -incRadius;
+		}
+		return radius;
+	}
+	
+	public LightSpot setOffsetX(int offsetX) {
+		this.offsetX = offsetX; 
+		return this;
+	}
+	
+	public LightSpot setOffsetY(int offsetY) {
+		this.offsetY = offsetY;
+		return this;
+	}
+
+	public LightSpot setOffset(int offsetX, int offsetY) {
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+		return this;
+	}
+	
+	public LightSpot setRadius(double radius)
+		{ return setRadiusVariance(radius, radius, 0); }
+
+	public static void clearTempLightSpots()
+		{ tempSpots.clear(); }
+	
+	public static void addTempLightSpot(LightSpot spot)
+		{ tempSpots.add(spot); }
+
+	public static void addLightSpot(LightSpot spot)
+		{ spots.add(spot); }
+	
+	public static void removeLightSpot(LightSpot spot)
+		{ spots.remove(spot); }
+	
+	public static void addLightSpotInDarkness(LightSpot spot)
+		{ spotsInDarkness.add(spot); }
+	
+	public static void removeLightSpotInDarkness(LightSpot spot)
+		{ spotsInDarkness.remove(spot); }
+
+	public static void setMultipleLightSpotsInDarkness(GraphicsContext gc)
+		{ setMultipleLightSpots(gc, true); }
+	
+	public static void setMultipleLightSpots(GraphicsContext gc)
+		{ setMultipleLightSpots(gc, false); }
+	
+	private static void setMultipleLightSpots(GraphicsContext gc, boolean startWithDarkness) {
+		gc.save();
+		Blend blend = null, blend2 = null;
+    for (int n = 0; n < 3; n++)
+	    for (LightSpot spot : n == 0 ? spots : n == 1 ? tempSpots : spotsInDarkness) {
+				blend2 = new Blend(blend == null && startWithDarkness ? null : BlendMode.ADD);
+				blend2.setTopInput(new Lighting(spot.getLightPoint()));
+				if (blend != null)
+					blend2.setBottomInput(blend);
+				blend = blend2;
+	    }
+    gc.applyEffect(blend);
+		gc.restore();
+	}
+	
+}
